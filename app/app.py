de01fb0a6071cd7b298db7fce863da09f98f5f1a
Diego Monsiváis <monsivaisdiego27@gmail.com>
2026-04-20 01:01:27 -0600

    Update home route message for clarity

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hola Diego 🚀 CI/CD funcionando"
+    return "Hola Diego CI/CD ya está funcionando"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
